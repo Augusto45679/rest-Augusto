41e2e35cbb41c4f9bcb6d0d1c91aa10431a376eb
Augusto Berloin <nfunes706@gmail.com>
2024-09-19 15:07:03 -0300

    15.06

--- src/main/java/com/example/demo/controllers/PersonaController.java
+++ src/main/java/com/example/demo/controllers/PersonaController.java
@@ -3,19 +3,12 @@ package com.example.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entities.Persona;
 import com.example.demo.services.PersonaService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @CrossOrigin(origins ="*") //con esto permitimos el ingreso a nuestra API de distintos origens
